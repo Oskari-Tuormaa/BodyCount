bce8e3ab5ad40c79f859874f00b3b650dac3f6ad
Oskari Tuormaa <oskaritu@gmail.com>
2022-07-10 16:09:40 +0200

    Change structure in module pricing and add category pricing

--- src/extras/classes.py
+++ src/extras/classes.py
@@ -141,16 +141,19 @@ class PriceCount(object):
     def __init__(self):
         self._prices: Dict[str, PriceObject] = dict()
         # Regex for removing version number from name
-        self._re_remove_version: re.Pattern = re.compile("^.*?(?=$| v\d+)")
+        self._re_remove_version: re.Pattern = re.compile(r"^.*?(?=$| v\d+)")
 
-    def add_to_ws(self, ws: pylightxl.Worksheet):
+    def add_to_db(self, ws: pylightxl.Worksheet):
         """Saves prices to worksheet"""
 
         # Populate headers
-        ws.update_index(2,  8, "Components")
-        ws.update_index(2,  9, "Price per")
-        ws.update_index(2, 10, "Count")
-        ws.update_index(2, 11, "Total price")
+        ws.update_index(2,  8, "Modules")
+        ws.update_index(2,  9, "Category")
+        ws.update_index(2, 10, "Price per")
+        ws.update_index(2, 11, "Count")
+        ws.update_index(2, 12, "Cost")
+        ws.update_index(2, 13, "Price ex. VAT")
+        ws.update_index(2, 14, "Price incl. VAT")
 
         # Add data per Price
         keys = list(self._prices.keys())
@@ -158,16 +161,27 @@ class PriceCount(object):
         for idx, k in enumerate(keys):
             v = self._prices[k]
             ws.update_index(3+idx,  8, k)
-            ws.update_index(3+idx,  9, v._price_per)
-            ws.update_index(3+idx, 10, v._count)
-            ws.update_index(3+idx, 11, f"=I{3+idx}*J{3+idx}")
+            ws.update_index(3+idx, 10, v._price_per)
+            ws.update_index(3+idx, 11, v._count)
+            ws.update_index(3+idx, 12, f"=J{3+idx}*K{3+idx}")
+            ws.update_index(3+idx, 13, f"=L{3+idx}*1.4")
+            ws.update_index(3+idx, 14, f"=M{3+idx}*1.25")
 
-        ws.update_index(5+idx, 10, "Total Price")
-        ws.update_index(6+idx, 10, "\w Markup (x1.4)")
-        ws.update_index(7+idx, 10, "\w Markup and Moms (x1.75)")
-        ws.update_index(5+idx, 11, f"=SUM(K3:K{3+idx})")  # Total price
-        ws.update_index(6+idx, 11, f"=K{5+idx}*1.4")  # Total price /w markup
-        ws.update_index(7+idx, 11, f"=K{6+idx}*1.25")  # Total price /w markup & moms
+        n_modules = len(keys)
+        categories = ["String 1", "String 2", "String 3", "Island", "Custom"]
+        for idx, c in enumerate(categories):
+            de_row = 2+n_modules # Data End row
+            row = 3+idx+n_modules # Current row
+            ws.update_index(row, 11, c)
+            ws.update_index(row, 12, fr"=SUMIF(I3:I{de_row},K{row},L3:L{de_row})")
+            ws.update_index(row, 13, fr"=SUMIF(I3:I{de_row},K{row},M3:M{de_row})")
+            ws.update_index(row, 14, fr"=SUMIF(I3:I{de_row},K{row},N3:N{de_row})")
+
+        row = 3+n_modules+len(categories)
+        ws.update_index(row, 11, "Total")
+        ws.update_index(row, 12, f"=SUM(L{row-len(categories)}:L{row-1})")
+        ws.update_index(row, 13, f"=SUM(M{row-len(categories)}:M{row-1})")
+        ws.update_index(row, 14, f"=SUM(N{row-len(categories)}:N{row-1})")
 
     def __add__(self, other):
         if type(other) == adsk.fusion.OccurrenceList:
