8a7dae5c9786e4a16f66b4c0ae82584db9ba641c
Oskari Tuormaa <oskaritu@gmail.com>
2025-01-17 21:02:58 +0100

    Add initial user_data command inputs

--- commands/settings/entry.py
+++ commands/settings/entry.py
@@ -2,7 +2,10 @@ import adsk.core
 import adsk.fusion
 import os
 
+from pathlib import Path
+
 from ...lib import fusionAddInUtils as futil
+from ...lib import settings_lib
 from ... import config
 
 app = adsk.core.Application.get()
@@ -55,4 +58,54 @@ def stop():
         panel.deleteMe()
 
 def command_created(args: adsk.core.CommandCreatedEventArgs):
-    ...
+    futil.log("Creating settings")
+
+    user_data = settings_lib.load_user_data()
+
+    inputs = args.command.commandInputs
+
+    inputs.addTextBoxCommandInput('txt1', '', '<h3><center>User Data</center></h3>', 1, True)
+    dropbox_path = inputs.addTextBoxCommandInput('dropbox_path', 'Path to Dropbox', '', 1, False)
+
+    inputs.addBoolValueInput('select_folder', 'Select Folder', False)
+
+    inputs.addSeparatorCommandInput('sep1')
+    inputs.addTextBoxCommandInput('txt2', '', '<h3><center>Shared Data</center></h3>', 1, True)
+
+    if user_data.shared_data_path is not None:
+        dropbox_path.text = user_data.shared_data_path
+
+    if user_data.shared_data_path is not None and Path(user_data.shared_data_path).exists():
+        ...
+    else:
+        inputs.addTextBoxCommandInput('wrn1', '', '<i><center>Set <b>Path to Dropbox</b> to a valid path to access shared settings</center></i>', 1, True)
+        inputs.addTextBoxCommandInput('wrn2', '', '<i><center>Note that command must be ok\'d and opened again for change to take effect.</center></i>', 2, True)
+
+    futil.add_handler(args.command.execute, command_execute)
+    futil.add_handler(args.command.inputChanged, input_changed)
+    futil.add_handler(args.command.validateInputs, validate_inputs)
+
+def validate_inputs(args: adsk.core.ValidateInputsEventArgs):
+    inputs = args.inputs
+    dropbox_path: adsk.core.TextBoxCommandInput = inputs.itemById('dropbox_path')
+    path = Path(dropbox_path.text)
+    args.areInputsValid = path.is_absolute() and path.is_dir()
+
+def input_changed(args: adsk.core.InputChangedEventArgs):
+    changed_input = args.input
+    if  changed_input.id == 'select_folder':
+        folder_dialog = ui.createFolderDialog()
+        folder_dialog.title = 'Select the DropBox root folder'
+        result = folder_dialog.showDialog()
+
+        if result == adsk.core.DialogResults.DialogOK:
+            dropbox_path = args.inputs.itemById('dropbox_path')
+            dropbox_path.text = folder_dialog.folder
+
+def command_execute(args: adsk.core.CommandEventArgs):
+    inputs = args.command.commandInputs
+    dropbox_path: adsk.core.TextBoxCommandInput = inputs.itemById('dropbox_path')
+
+    user_data = settings_lib.load_user_data()
+    user_data.shared_data_path = dropbox_path.text
+    settings_lib.save_user_data(user_data)
