fc49578ac3e4aebba48a1bbc19efda3365ff3ed6
Oskari Tuormaa <oskaritu@gmail.com>
2025-01-17 20:10:54 +0100

    Fill out start and stop functions

--- commands/settings/entry.py
+++ commands/settings/entry.py
@@ -22,5 +22,37 @@ ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resource
 def start():
     futil.log("Hello from settings")
 
+    cmd_def = ui.commandDefinitions.addButtonDefinition(CMD_ID, CMD_NAME, CMD_DESCRIPTION, ICON_FOLDER)
+
+    futil.add_handler(cmd_def.commandCreated, command_created)
+
+    workspace = ui.workspaces.itemById(WORKSPACE_ID)
+
+    # Create panel if it doesn't already exist
+    if (panel := workspace.toolbarPanels.itemById(PANEL_ID)) is None:
+        panel = workspace.toolbarPanels.add(PANEL_ID, "BodyCount")
+
+    control = panel.controls.addCommand(cmd_def)
+    control.isPromoted = IS_PROMOTED
+
 def stop():
+    futil.log("Goodbye from settings")
+
+    cmd_def = ui.commandDefinitions.itemById(CMD_ID)
+
+    workspace = ui.workspaces.itemById(WORKSPACE_ID)
+    panel = workspace.toolbarPanels.itemById(PANEL_ID)
+    control = panel.controls.itemById(CMD_ID)
+
+    if cmd_def:
+        cmd_def.deleteMe()
+
+    if control:
+        control.deleteMe()
+
+    # Delete panel if this was the last command
+    if panel and len(panel.controls) == 0:
+        panel.deleteMe()
+
+def command_created(args: adsk.core.CommandCreatedEventArgs):
     ...
