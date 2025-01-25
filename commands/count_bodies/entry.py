547c6adf0180b90633b9f767c2cf1b94f5fb447d
Oskari Tuormaa <oskaritu@gmail.com>
2025-01-25 16:47:23 +0100

    Add materials table and excel path inputs to count_bodies

--- commands/count_bodies/entry.py
+++ commands/count_bodies/entry.py
@@ -7,11 +7,10 @@ import openpyxl as op
 from openpyxl.workbook import Workbook
 
 from ...lib import fusionAddInUtils as futil
-from ...lib import counting_lib
-from ...lib import excel_lib
+from ...lib import counting_lib, excel_lib, settings_lib
 from ... import config
 
-from ...lib import custom_graphics_lib
+from pathlib import Path
 
 app = adsk.core.Application.get()
 ui = app.userInterface
@@ -28,8 +27,6 @@ PANEL_ID = "BodyCount"
 ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "")
 
 ATTR_GRP = f"{config.COMPANY_NAME}_{config.ADDIN_NAME}"
-
-selgroups = custom_graphics_lib.SelectionGraphicsGroups()
 
 
 def start():
@@ -119,22 +116,98 @@ def command_created(args: adsk.core.CommandCreatedEventArgs):
     design = adsk.fusion.Design.cast(product)
     rootComp = design.rootComponent
 
-    bodies = counting_lib.collect_bodies_under(rootComp)
+    inputs = args.command.commandInputs
+
+    shared_data = settings_lib.load_shared_data()
     modules = counting_lib.collect_modules_under(rootComp)
 
-    input_file_path = get_input_file_path()
-    if not input_file_path:
-        return
+    path_table = inputs.addTableCommandInput('', '', 3, '3:6:2')
+    excel_file_path = inputs.addStringValueInput('excel_path', '', '')
+    select_button = inputs.addBoolValueInput('select_folder', 'Select Folder', False)
+    path_table.addCommandInput(inputs.addTextBoxCommandInput('', '', '<h4>Path to excel file</h4>', 1, True), 0, 0)
+    path_table.addCommandInput(excel_file_path, 0, 1)
+    path_table.addCommandInput(select_button, 0, 2)
+    path_table.tablePresentationStyle = adsk.core.TablePresentationStyles.transparentBackgroundTablePresentationStyle
+    path_table.minimumVisibleRows = 1
+    path_table.maximumVisibleRows = 1
 
-    workbook = op.open(input_file_path, keep_vba=True, keep_links=True, rich_text=True)
+    module_table = inputs.addTableCommandInput('modules', 'Modules', 3, '1:1:1')
+    module_table.addCommandInput(inputs.addTextBoxCommandInput('', '', '<h3>Category</h3>', 1, True), 0, 0)
+    module_table.addCommandInput(inputs.addTextBoxCommandInput('', '', '<h3>Detail material</h3>', 1, True), 0, 1)
+    module_table.addCommandInput(inputs.addTextBoxCommandInput('', '', '<h3>Wood material</h3>', 1, True), 0, 2)
+
+    groups = {module.category for module in modules}
+    for i, group in enumerate(groups):
+        name_inp = inputs.addTextBoxCommandInput('', '', group, 1, True)
+        detail_dropdown = inputs.addDropDownCommandInput('', '', adsk.core.DropDownStyles.TextListDropDownStyle)
+        wood_dropdown = inputs.addDropDownCommandInput('', '', adsk.core.DropDownStyles.TextListDropDownStyle)
+
+        for detail_material in shared_data.detail_materials:
+            detail_dropdown.listItems.add(detail_material, False)
+
+        for wood_material in shared_data.wood_materials:
+            wood_dropdown.listItems.add(wood_material, False)
+
+        module_table.addCommandInput(name_inp, i+1, 0)
+        module_table.addCommandInput(detail_dropdown, i+1, 1)
+        module_table.addCommandInput(wood_dropdown, i+1, 2)
+
+    futil.add_handler(args.command.execute, command_execute)
+    futil.add_handler(args.command.inputChanged, input_changed)
+    futil.add_handler(args.command.validateInputs, validate_inputs)
+
+def validate_inputs(args: adsk.core.ValidateInputsEventArgs):
+    inputs = args.inputs
+    wood_table: adsk.core.TableCommandInput = inputs.itemById('modules')
+
+    dropdowns_filled = True
+    for i in range(1, wood_table.rowCount):
+        detail_dropdown: adsk.core.DropDownCommandInput = wood_table.getInputAtPosition(i, 1)
+        wood_dropdown: adsk.core.DropDownCommandInput = wood_table.getInputAtPosition(i, 2)
+
+        if detail_dropdown.selectedItem is None or wood_dropdown.selectedItem is None:
+            dropdowns_filled = False
+            break
+
+    excel_path_inp: adsk.core.StringValueCommandInput = inputs.itemById('excel_path')
+    excel_path = Path(excel_path_inp.value)
+    is_valid_excel_path = excel_path.is_absolute() and excel_path.is_file()
+    
+    args.areInputsValid = dropdowns_filled and is_valid_excel_path
+
+
+def input_changed(args: adsk.core.InputChangedEventArgs):
+    changed_input = args.input
+    inputs = args.inputs
+
+    if changed_input.id == 'select_folder':
+        new_path = get_input_file_path()
+        if new_path is None:
+            return
+        excel_path_input: adsk.core.StringValueCommandInput = inputs.itemById('excel_path')
+        excel_path_input.value = new_path
+
+
+def command_execute(args: adsk.core.CommandEventArgs):
+    product = app.activeProduct
+    design = adsk.fusion.Design.cast(product)
+    rootComp = design.rootComponent
+
+    inputs = args.command.commandInputs
+
+    bodies  = counting_lib.collect_bodies_under(rootComp)
+    modules = counting_lib.collect_modules_under(rootComp)
+
+    excel_path_input: adsk.core.StringValueCommandInput = inputs.itemById('excel_path')
+    excel_path = excel_path_input.value
+
+    workbook = op.open(excel_path, keep_vba=True, keep_links=True, rich_text=True)
 
     excel_lib.write_bodies_to_table(workbook, bodies)
     excel_lib.write_modules_to_table(workbook, modules)
 
-    output_file_path = get_output_file_path()
-    if not output_file_path:
-        return
+    # output_file_path = get_output_file_path()
+    # if not output_file_path:
+    #     return
 
-    try_saving_workbook(workbook, output_file_path)
-
-    futil.log("Done")
+    try_saving_workbook(workbook, excel_path)
